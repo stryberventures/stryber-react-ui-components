1f196e1d4c2ff2b4122633e0eebac8992ce47f3b
Andrii Balaniuk <andrii@stryber.com>
2019-11-06 18:18:33 +0200

    Fix form props

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -39,7 +39,7 @@ interface FormProps {
 }
 
 /** Form component */
-const Form = (props: FormProps & React.HTMLProps<HTMLFormElement>) => {
+const Form = (props: FormProps) => {
   const {
     children,
     onSubmit,
