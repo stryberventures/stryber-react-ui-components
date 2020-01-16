5f8432aa087249566659f8f0d0c78738b7f99599
Vlad Beltsar <vlad@stryber.com>
2020-01-16 13:42:41 +0200

    Changed "ButtonSet" component

--- src/components/ButtonsSet/ButtonsSet.styles.ts
+++ src/components/ButtonsSet/ButtonsSet.styles.ts
@@ -2,7 +2,8 @@ export default (theme: any) => ({
   root: {
     border: `1px solid ${theme.buttonBackgroundColorPrimary}`,
     display: "inline-block",
-    borderRadius: 6
+    borderRadius: 6,
+    position: 'relative'
   },
   button: {
     display: "inline-block",
@@ -12,6 +13,23 @@ export default (theme: any) => ({
     padding: 6,
     textOverflow: "ellipsis",
     overflow: "hidden",
-    whiteSpace: "nowrap"
+    whiteSpace: "nowrap",
+    backgroundColor: 'transparent'
+  },
+  active: {
+    color: '#fff',
+    transition: 'color 0.2s ease',
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  background: {
+    backgroundColor: theme.buttonBackgroundColorPrimary,
+    width: 71,
+    height: 24,
+    position: 'absolute',
+    zIndex: -1,
+    borderRadius: 4,
+    transition: 'left 0.2s ease'
   }
 });
