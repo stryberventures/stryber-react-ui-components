9b987024484570b0da6ab3f8b771b085f8778b58
Sergiy Ostrovsky <sergiy.ostrovsky@stryber.com>
2020-04-30 14:46:57 +0300

    Added onEnter to <SearchField />

--- src/components/SearchField/SearchField.stories.tsx
+++ src/components/SearchField/SearchField.stories.tsx
@@ -10,22 +10,26 @@ storiesOf('Search Field', module)
       <Wrapper>
         <SearchField
           onChange={ (d: any) => console.log('SearchBox value:', d) }
+          onEnter={ value => console.log(`onEnter('${value}')`)}
         />
         <SearchField
           collapsiblePlaceholder={false}
           onChange={ (d: any) => console.log('SearchBox value:', d) }
+          onEnter={ value => console.log(`onEnter('${value}')`)}
         />
         <SearchField
           sizeVariant='mini'
           placeholder='Search mini'
           collapsiblePlaceholder={false}
           onChange={ (d: any) => console.log('SearchBox value:', d) }
+          onEnter={ value => console.log(`onEnter('${value}')`)}
         />
         <SearchField
           sizeVariant='small'
           placeholder='Search small'
           collapsiblePlaceholder={false}
           onChange={ (d: any) => console.log('SearchBox small value:', d) }
+          onEnter={ value => console.log(`onEnter('${value}')`)}
         />
       </Wrapper>
     );
