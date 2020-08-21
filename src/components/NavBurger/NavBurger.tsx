566e8df851a7c29dfde4fdf14b22a93ddb3fa808
Vlad Beltsar <vlad@stryber.com>
2020-08-21 18:03:28 +0300

    Added animation to Navburger component

--- src/components/NavBurger/NavBurger.tsx
+++ src/components/NavBurger/NavBurger.tsx
@@ -3,13 +3,14 @@ import classNames from 'classnames';
 import withStyles, { WithStyles } from 'react-jss';
 
 import styles from './NavBurger.styles';
-import { Hamburger } from '../Icons';
+import { useState } from 'react';
 
 
 interface ITagProps {
   onClick?: (e: React.SyntheticEvent) => void;
   className?: any;
   active?: boolean;
+  animationDuration?: number;
 }
 
 const NavBurger = (props: ITagProps & React.HTMLAttributes<HTMLDivElement> & WithStyles<typeof styles>) => {
@@ -17,16 +18,32 @@ const NavBurger = (props: ITagProps & React.HTMLAttributes<HTMLDivElement> & Wit
     classes,
     active,
     onClick,
+    animationDuration = 0.5,
     ...rest
   } = props;
+
+  const [animation, setAnimation] = useState(false);
+
   return (
     <div
       className={classNames([classes.root, classNames])}
-      onClick={onClick}
+      onClick={(e) => {
+        setAnimation(true);
+        onClick && onClick(e);
+      }}
       {...rest}
     >
-      <div className={classes.navburgerWrapper}>
-        <Hamburger />
+      <div className={classNames(classes.navburgerWrapper)}>
+        <svg className={classNames({ [classes.inactiveBurger]: animation }, { [classes.activeBurger]: active })} width="24px" height="24px" viewBox="0 0 49 40" version="1.1">
+          <title>icon</title>
+          <desc>Created with Sketch.</desc>
+          <g id="icon" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <rect id="topRectangle" fill="#000000" x="3" y="3" width="42" height="4"></rect>
+            <rect id="middleRectangle" className={classes.middleRectangle} fill="#000000" x="3" y="17" width="21" height="4"></rect>
+            <rect id="middleRectangle2" className={classNames(classes.middleRectangle2, classes.middleRectangle)} fill="#000000" x="24" y="17" width="21" height="4"></rect>
+            <rect id="bottomRectangle" fill="#000000" x="3" y="31" width="42" height="4"></rect>
+          </g>
+        </svg>
         {active && (<div className={classes.octagonWrapper}>
           <div className={classes.octagon}>
             <div className={classes.octagonInner}/>
